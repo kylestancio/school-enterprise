1cbb5af68db7604060637ad79840aac4899e857d
kylestancio <kylestancio@gmail.com>
2023-10-05 14:10:05 +0900

    [feat] change navigation links from button to link

--- manage/app/Headers.tsx
+++ manage/app/Headers.tsx
@@ -2,13 +2,11 @@
 
 import ThemeButton from '@/components/ThemeButton'
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export default function Headers() {
-
-  const router = useRouter();
-
   return (
     <header className='w-full h-20 bg-zinc-100 dark:bg-zinc-900 shadow-md flex gap-5 px-5'>
       <div className='my-auto'>
@@ -16,8 +14,9 @@ export default function Headers() {
         <p className='text-sm'>Management Console</p>
       </div>
       <div className='grow my-auto'>
-        <Button variant={'link'} onClick={()=>router.push('/')}>Home</Button>
-        <Button variant={'link'} onClick={()=>router.push('/employees')}>Employees</Button>
+        <Link href={'/'} className='text-sm font-medium hover:underline underline-offset-4 me-5'>Home</Link>
+        <Link href={'/employees'} className='text-sm font-medium hover:underline underline-offset-4 me-5'>Employees</Link>
+        <Link href={'/courses'} className='text-sm font-medium hover:underline underline-offset-4 me-5'>Courses</Link>
       </div>
       <div className='my-auto'>
         <ThemeButton />
