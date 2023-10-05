a07d1415d620fc11cdd357b241c26eeb0025be4a
kylestancio <kylestancio@gmail.com>
2023-10-05 14:19:45 +0900

    [feat] add role-based access

--- manage/app/api/user/view/route.ts
+++ manage/app/api/user/view/route.ts
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
+import { User, getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface IRequestBody{
   id: string
@@ -7,6 +9,16 @@ interface IRequestBody{
 
 export async function GET(req: NextRequest){
 
+  try{
+    const session = await getServerSession(authOptions)
+    if (!session) throw new Error("User is not logged in")
+    const { user } = session
+    if (!user.roles.includes('EMPLOYEESMANAGER')) throw new Error("User does not have proper role")
+  }catch(err){
+    console.error(err)
+    return NextResponse.error()
+  }
+  
   const searchParams = new URL(req.url).searchParams
   const id = searchParams.get('id')
 
